a3e91b7039dc384bae51be490984f468e688fc3f
Anish kumar <94589270+Anishkumar31@users.noreply.github.com>
2022-03-05 05:18:06 +0000

    p1 done

---
+++ p2original.c
@@ -0,0 +1,14 @@
+typedef struct_fraction
+{
+   int num,den;
+}Fraction;
+Fraction input_fraction(){
+  Fraction n,d;
+  printf("Enter the numerator : ");
+  scanf("%d",&Fraction.n);
+  printf("Enter the denominator : ");
+  scanf("%d",&Fraction.d);
+}
+Fraction largest_fraction(Fraction f1,Fraction f2,Fraction f3){
+  
+}
